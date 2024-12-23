ecdf0dff416086665fc7b3123b56e0ba090ef02c
Soumyadeep <Soumyadeep1211@outlook.com>
2024-12-24 00:17:03 +0530

    print menu is committed

--- ToDoListApp.java
+++ ToDoListApp.java
@@ -41,3 +41,13 @@ public class ToDoListApp {
             handleChoice(choice);
         }
     }
+
+    private static void printMenu() {
+        System.out.println("\nMenu:");
+        System.out.println("1. Add Task");
+        System.out.println("2. View Tasks");
+        System.out.println("3. Mark Task as Completed");
+        System.out.println("4. Delete Task");
+        System.out.println("5. Exit");
+        System.out.print("Choose an option: ");
+    }
